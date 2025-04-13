1530e54fcec54cb6e6334577d33bbe2f611aa8ee
heshteo <hesh.teo@gmail.com>
2025-04-13 02:13:30 +0200

    r

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
             <HomePage />
             <About/>
             <Services/>
-            <FAQ/>
+            {/* <FAQ/> */}
             <Reviews/>
             <Contact/>
             <Footer/>
